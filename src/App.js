34afe40c9229b8d407690fa04414ec9b5cd8aac9
edmondokosun <angelehimare1@gmail.com>
2019-11-21 22:33:25 +0100

    added the loading header when robots length === 0

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
-      .then(users => this.setState({ robots: users }));
+      .then(users => {this.setState({ robots: users })});
   }
 
   onSearchChange = event => {
@@ -28,14 +28,17 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-
-    return (
-      <div className="tc pa3">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList Robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc pa3">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList Robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
